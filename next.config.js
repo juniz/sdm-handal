4b4dd863ab548b245ee7d7231c4fb3a4a8d888bc
Yudo <53389941+juniz@users.noreply.github.com>
2025-05-16 13:13:38 +0700

    Menambahkan konfigurasi webpack di next.config.js untuk menonaktifkan alias canvas dan encoding, meningkatkan kompatibilitas proyek.

--- next.config.js
+++ next.config.js
@@ -23,6 +23,11 @@ const nextConfig = {
 			},
 		],
 	},
+	webpack: (config) => {
+		config.resolve.alias.canvas = false;
+		config.resolve.alias.encoding = false;
+		return config;
+	},
 	experimental: {
 		serverActions: true,
 	},
